13e30dc42e76326116d629585d54b2fa684186ba
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-03 14:29:13 +0000

    [skip actions] lab/lab01/ch01_t13_value_error.py

--- lab/lab01/ch01_t13_value_error.py
+++ lab/lab01/ch01_t13_value_error.py
@@ -2,4 +2,4 @@ float_1 = 0.25
 float_2 = 40.0
 
 product = float_1 * float_2
-big_string = "The product of " + str(float_1) + " and " + str(float_2) + " is " + str(product)
+big_string = "The product was " + str(float_1) + " and " + str(float_2) + " is " + str(product)
