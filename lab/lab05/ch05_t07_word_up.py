8c59d0a76e98c2cfb80373ebc111c907303fe388
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-23 04:12:05 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -3,7 +3,8 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
-    
+    word = original.lower()
+    first
     print(original)
 else:
     print('empty')
