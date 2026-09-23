e87c0b168fb267676a03bfffc90a2bed6fe2384d
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-23 13:28:26 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

--- lab/lab06/ch06_t17_review_functions.py
+++ lab/lab06/ch06_t17_review_functions.py
@@ -1,8 +1,7 @@
-def shut_doem(s):
+def shut_down(s):
     if s == "yes":
         print("Shutting down")
     elif s == "no":
         print("Shutdown aborted")
     else:
         print("Sorry")
-shut_doem()
