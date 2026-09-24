06d251e91ecef37f23c39e0ea1b3eced992ed289
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-24 08:44:11 +0000

    [skip actions] lab/lab08/ch08_t14_last_note.py

--- lab/lab08/ch08_t14_last_note.py
+++ lab/lab08/ch08_t14_last_note.py
@@ -12,6 +12,6 @@ inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
 inventory['pouch'].sort()
 
 # Your code here
-inventory['pocket']=[strings 'seashell', 'strange berry', 'lint']
+inventory['pocket']=['seashell', 'strange berry', 'lint']
 inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
