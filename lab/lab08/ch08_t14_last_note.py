28b08b6f0c5ae18d31c10979076b587360a6e794
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-24 08:45:18 +0000

    [skip actions] lab/lab08/ch08_t14_last_note.py

--- lab/lab08/ch08_t14_last_note.py
+++ lab/lab08/ch08_t14_last_note.py
@@ -15,4 +15,4 @@ inventory['pouch'].sort()
 inventory['pocket']=['seashell', 'strange berry', 'lint']
 inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
-inventory['backpack']
+inventory['gold']
