8f9484b7be598c5079a2e34c92599c35e45a0a6c
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-24 07:40:20 +0000

    [skip actions] lab/lab08/ch08_t07_maintaining_order.py

--- lab/lab08/ch08_t07_maintaining_order.py
+++ lab/lab08/ch08_t07_maintaining_order.py
@@ -2,6 +2,6 @@ animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
 duck_index = [2]  # Use index() to find "duck"
 
 # Your code here!
-animals.insert(, item) 
+animals.insert(2, item) 
 
 print(animals)  # Observe what prints after the insert operation
