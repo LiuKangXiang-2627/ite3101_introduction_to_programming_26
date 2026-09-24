ca984fde77aef99e0cdd397b57f602e7b69509bd
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-24 07:25:08 +0000

    [skip actions] lab/lab08/ch08_t05_list_slicing.py

--- lab/lab08/ch08_t05_list_slicing.py
+++ lab/lab08/ch08_t05_list_slicing.py
@@ -4,7 +4,7 @@ suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
 first = suitcase[0:2]
 
 # Third and fourth items (index two and three)
-middle = [2:4]
+middle = [2:3]
 
 # The last two items (index four and five)
-last = [4:6]
+last = [4:5]
