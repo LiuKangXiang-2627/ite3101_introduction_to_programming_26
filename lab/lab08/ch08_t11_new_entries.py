1ce741e07384b67a93512e864ca3d162a9cab43f
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-24 08:16:09 +0000

    [skip actions] lab/lab08/ch08_t11_new_entries.py

--- lab/lab08/ch08_t11_new_entries.py
+++ lab/lab08/ch08_t11_new_entries.py
@@ -4,6 +4,7 @@ print(menu['Chicken Alfredo'])
 
 # Your code here: Add some dish-price pairs to menu!
 menu['Spam'] = 2.50
-menu['chicken'] = 2.50
+menu['chicken'] = 15
+
 print("There are " + str(len(menu)) + " items on the menu.")
 print(menu)
