4b92a0325adb0cd6f43d60c57669a1ae19325818
LiuKangXiang-2627 <kliu39311@gmail.com>
2026-09-24 03:29:43 +0000

    [skip actions] lab/lab07/ch07_t03_getting_there.py

--- lab/lab07/ch07_t03_getting_there.py
+++ lab/lab07/ch07_t03_getting_there.py
@@ -1,9 +1,9 @@
 def plane_ride_cost(city):
     if city == 'Charlotte':
-        teturn 183
+        teturn '183'
     elif city == 'Tampa':
-        return 220
+        return '220'
     elif city == 'Pittsburgh':
-        return 222
+        return '222'
     elif city == 'Los Angeles':
-        return 475
+        return '475'
